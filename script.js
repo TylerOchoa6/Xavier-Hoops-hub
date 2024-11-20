17b85e7e11d7b0a746ec5576ff8f680717acea48
TylerOchoa6 <ochoat@xavier.edu>
2024-11-20 12:18:03 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Fan Zone interaction
+document.querySelector("button").addEventListener("click", () => {
+  const comment = document.querySelector("textarea").value;
+  alert(`Your comment: "${comment}" has been submitted!`);
+  document.querySelector("textarea").value = "";
+});
